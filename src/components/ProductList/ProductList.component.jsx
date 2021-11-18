f3f3caad7e2acc0d5ef36bd73ed3ed505b26a7e9
Francisco Rodriguez <Francisco.Rodriguez.-ND@disney.com>
2021-11-18 15:58:13 -0700

    Create new ProductList component (returns h1 and text for now).

---
+++ src/components/ProductList/ProductList.component.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const ProductListComponent = () => {
+    return (
+        <h1>
+           This is the Product List Page. 
+        </h1>
+    )
+};
+
+export default ProductListComponent;
---
+++ src/components/ProductList/ProductList.styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import ProductListComponent from './ProductList.component'
+
+const ProductList = styled(ProductListComponent)``;
+
+export default ProductList;
