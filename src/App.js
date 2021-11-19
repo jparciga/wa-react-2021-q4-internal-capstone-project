a7bd282b863418d24eb91cc043b21e88a45c5a2f
Francisco Rodriguez <Francisco.Rodriguez.-ND@disney.com>
2021-11-18 21:25:09 -0700

    Implement conditional rendering. Implement logo redirect to Home. Implement click event handler.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import './App.css';
 import { Container, Logo, Header, Search, ShoppingCart, Content, Footer} from './App.style';
+import { useState, useEffect } from 'react';
 
 import Home from 'components/Home/Home';
+import ProductList from 'components/ProductList/ProductList.styles'
 
 import LogoImg from 'images/image.png';
 import ShoppingCartIcon from 'images/shopping-cart.svg';
 
 const App = () => {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  const handleNavigation = () => {
+    setCurrentPage((currentPage === "Home") ? "ProductList" : "Home");
+  };
+
   return (
     <Container>
       <Header>
         <Logo>
-          <img src={LogoImg} alt="logo" height="100%"></img>
+          <img src={LogoImg} onClick={() => setCurrentPage("Home")} style={{cursor: "pointer"}} alt="logo" height="100%"></img>
         </Logo>
         <Search>
           <input type="text" disabled></input>
         </Search>
         <ShoppingCart>
-          <img src={ShoppingCartIcon} alt="shoppingcart"></img>
+          <img src={ShoppingCartIcon}  alt="shoppingcart"></img>
         </ShoppingCart>
       </Header>
       <Content>
-        <Home />
+      {currentPage === "Home" ? <Home /> : <ProductList />}
         <div>
-          <button id="btnChangePage">View All Products</button>
+          <button id="btnChangePage" onClick={handleNavigation}>View All Products</button>
         </div>
       </Content>
       <Footer>Ecommerce created during Wizeline’s Academy React Bootcamp.</Footer>
