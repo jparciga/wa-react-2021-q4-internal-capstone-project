149c1f27050bcc65dd6c6ac5f8969563a11f8be1
Francisco Rodriguez <Francisco.Rodriguez.-ND@disney.com>
2021-11-23 23:12:49 -0700

    Fix bugs related to the hooks.

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -2,27 +2,29 @@ import { useState, useEffect } from "react";
 
 const useData = (fileName, mapFunction) => {
     const [parsedData, setParsedData] = useState([]);
-
-    const convertData = (data) => {
-        const results = data.results;
-
-        var productCategoriesConverted = results.map(mapFunction);
-        setParsedData(productCategoriesConverted);
-    }
    
     useEffect(() => {
-        fetch(fileName, {
+        const abortController = new AbortController();
+
+        fetch(`wa-react-2021-q4-internal-capstone-project/${fileName}`, {
             headers : { 
               'Content-Type': 'application/json',
               'Accept': 'application/json'
              }
            })
             .then(response => response.json())
-            .then(convertData)
+            .then((data) => {
+                const results = data.results;
+        
+                var productCategoriesConverted = results.map(mapFunction);
+                setParsedData(productCategoriesConverted);
+            })
             .catch(error => {
                 console.error('There has been a problem with your fetch operation:', error);
             });
-    });
+
+        return () => abortController.abort();
+    }, []);
     
     return [parsedData];
 }
--- src/hooks/useFeaturedBanners.js
+++ src/hooks/useFeaturedBanners.js
@@ -1,12 +1,11 @@
 import useData from 'hooks/useData';
 
-const useFeaturedProducts = () => {
+const useFeaturedBanners = () => {
     const fileName = 'featured-banners.json';
-    //key={obj.id} url={obj.data.main_image.url}
     const mapFunction = ({id, data: { main_image: { url } }}) => { return { id, url } };
 
     const [productCategories] = useData(fileName, mapFunction);
     return [productCategories];
 };
 
-export default useFeaturedProducts;
+export default useFeaturedBanners;
