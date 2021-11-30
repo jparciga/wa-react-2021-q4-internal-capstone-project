0ae8bfd146a6784ff1d49f5df2ec4864ffe00568
Francisco Rodriguez <Francisco.Rodriguez.-ND@disney.com>
2021-11-30 16:57:52 -0700

    Extract React Router logic to its own component. Remove redundant Redirect component.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import './App.css';
 import { Container, Logo, Header, Search, ShoppingCart, Content, Footer} from './App.style';
-import { useState } from 'react';
 
-import Home from 'components/Home/Home';
-import ProductList from 'components/ProductList/ProductList.styles'
-import ProductDetail from 'components/ProductDetail/ProductDetail.component';
+import PageRouter from 'components/PageRouter/PageRouter.component';
 
 import LogoImg from 'images/image.png';
 import ShoppingCartIcon from 'images/shopping-cart.svg';
 
-import {Switch, Route, Link, Redirect } from 'react-router-dom';
+import {Link } from 'react-router-dom';
 
 const App = () => {
-  const [currentPage, setCurrentPage] = useState("Home");
 
-  const handleNavigation = () => {
-    setCurrentPage((currentPage === "Home") ? "ProductList" : "Home");
-  };
+
+
 
   return (
     <Container>
@@ -37,27 +32,7 @@ const App = () => {
         </ShoppingCart>
       </Header>
       <Content>
-        <Switch>
-          <Route exact path='/home'>
-            <Home />
-          </Route>
-          <Route exact path='/products'>
-            <ProductList />
-          </Route>
-          <Route exact path='/product/:productId'>
-            <ProductDetail></ProductDetail>
-          </Route>
-          <Route path='*'>
-            <span>404 Not Found.</span>
-          </Route>
-        </Switch>
-        <Link to={(currentPage === "Home") ? '/products' : '/home'}>
-          <button id="btnChangePage" onClick={handleNavigation}>View All Products</button>
-        </Link>
-
-        <Redirect exact from='/wa-react-2021-q4-internal-capstone-project' to='/home'  />
-        <Redirect exact from='/' to='/home' />
-        <Redirect exact from='/home' to='/home' />
+        <PageRouter />
       </Content>
       <Footer>Ecommerce created during Wizeline’s Academy React Bootcamp.</Footer>
     </Container>
---
+++ src/components/PageRouter/PageRouter.component.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+import { Switch, Route, Link, Redirect } from "react-router-dom";
+import Home from "components/Home/Home";
+import ProductList from "components/ProductList/ProductList.styles";
+import ProductDetail from "components/ProductDetail/ProductDetail.component";
+
+const PageRouter = () => {
+    const [currentPage, setCurrentPage] = useState("Home");
+
+    const handleNavigation = () => {
+        setCurrentPage((currentPage === "Home") ? "ProductList" : "Home");
+      };
+
+  return (
+    <>
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/products" component={ProductList} />
+        <Route exact path="/product/:productId" component={ProductDetail} />
+        <Route path="*">
+          <span>404 Not Found.</span>
+        </Route>
+      </Switch>
+      <Link to={currentPage === "Home" ? "/products" : "/home"}>
+        <button id="btnChangePage" onClick={handleNavigation}>
+          View All Products
+        </button>
+      </Link>
+
+      <Redirect
+        exact
+        from="/wa-react-2021-q4-internal-capstone-project"
+        to="/home"
+      />
+      <Redirect exact from="/" to="/home" />
+    </>
+  );
+}
+
+export default PageRouter;
