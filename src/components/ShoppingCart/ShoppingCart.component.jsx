5f0f9895de45c4d1b797e8b10f71a6f2a683004c
Francisco Rodriguez <Francisco.Rodriguez.-ND@disney.com>
2021-12-09 20:42:22 -0700

    Refactor calculation for Total Price to its own function.

--- src/components/ShoppingCart/ShoppingCart.component.jsx
+++ src/components/ShoppingCart/ShoppingCart.component.jsx
@@ -5,8 +5,12 @@ import ShoppingCartTable from './ShoppingCartTable/ShoppingCartTable.styles';
 import { useSelector } from 'react-redux';
 
 const ShoppingCartComponent = ({className}) => {
-
     const { items } = useSelector((state) => state.shoppingCart);
+
+    const calculateTotalPrice = (items) => {
+        const totalPrice = items.reduce( (previous, current) => previous + (current.price*current.quantity),0);
+        return totalPrice;
+    }
 
     return (
         <div className={className}>
@@ -14,7 +18,7 @@ const ShoppingCartComponent = ({className}) => {
             <div></div>
             <ShoppingCartTable />
             <div></div>
-            <div className="shoppingCart-total">Total: ${ items.reduce( (previous, current) => previous + (current.price*current.quantity),0) }</div>
+            <div className="shoppingCart-total">Total: ${ calculateTotalPrice(items) }</div>
             <div className="shoppingCart-checkout"><button>Checkout</button></div>            
         </div>
     )
