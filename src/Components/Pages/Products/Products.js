009de7784915d19fe0a4bbd717143e2ad25e9741
Alexander Sousa <566658+anesed@users.noreply.github.com>
2021-12-14 16:17:59 -0600

    read category slugs in Products page

--- src/Components/Pages/Products/Products.js
+++ src/Components/Pages/Products/Products.js
@@ -1,6 +1,6 @@
 import ProductGrid from 'Components/ProductGrid';
 import useProductsWithCategory from 'Utils/Hooks/useProductsWithCategory';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CategorySelector from 'Components/CategorySelector';
 import Paginator from 'Components/Paginator';
 import { Wrapper, CategorySection, ProductSection } from './Products.styles';
@@ -11,11 +11,27 @@ import useProducts from 'Utils/Hooks/useProducts';
 
 export default function Products() {
     const [params] = useSearchParams();
-    const queryCategories = params.getAll('category');
     const { data: { results: categories = [] } } = useCategories();
     const [{ data: { page, total_pages: pages, results: products = [] } }, setPage] = useProducts();
     const fullProducts = useProductsWithCategory(products, categories);
-    const [activeCategories, setActiveCategories] = useState(() => new Set(queryCategories));
+    const [activeCategories, setActiveCategories] = useState(() => new Set());
+    useEffect(() => {
+        const slugs = params.getAll('category');
+        if (slugs.size === 0) {
+            return () => {};
+        }
+        const slugMap = new Map();
+        for (const category of categories) {
+            for (const slug of category.slugs) {
+                if (!slugMap.has(slug)) {
+                    slugMap.set(slug, category.id);
+                }
+            }
+        }
+
+        const categoryIds = slugs.filter((slug) => slugMap.has(slug)).map((slug) => slugMap.get(slug));
+        setActiveCategories(new Set(categoryIds));
+    }, [params, categories]);
     const displayProducts = productsHavingCategory(fullProducts, activeCategories);
     const clearFilters = () => {
         setActiveCategories(new Set([]));
