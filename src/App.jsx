b1f3eab23b02c325c03c9f69688c58d46c2102af
Francisco Aceves <francisco.aceves@wizeline.com>
2021-12-14 15:09:26 -0600

    Removed console.log and comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useFeaturedBanners } from "./utils/hooks/useFeaturedBanners";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
 import { Outlet } from "react-router-dom";
 import { CartProvider } from "./contexts/CartContext";
 
 function App() {
-  const { data, isLoading } = useFeaturedBanners();
-  console.log(data, isLoading);
-
   return (
     <main>
       <CartProvider>
