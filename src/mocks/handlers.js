794190d91214af324f56a9c33067e3b324b69507
Francisco Aceves <francisco.aceves@wizeline.com>
2021-12-17 09:57:50 -0600

    Added filters to the mock handlers

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -12,23 +12,61 @@ const handlers = [
   rest.get(API_BASE_URL, (_, res, ctx) =>
     res(ctx.json({ refs: [{ ref: mockApiRef }] }))
   ),
+
   rest.get(`${API_BASE_URL}/documents/search`, (req, res, ctx) => {
-    const queries = req.url.searchParams.getAll("q");
-    if (queries[0] === '[[at(document.type, "banner")]]') {
+    const [query, options] = req.url.searchParams.getAll("q");
+    if (query === '[[at(document.type, "banner")]]') {
       return res(ctx.json(featuredBanners));
     }
-    if (queries[0] === '[[at(document.type, "category")]]') {
+
+    if (query === '[[at(document.type, "category")]]') {
       return res(ctx.json(productCategories));
     }
-    if (
-      queries[0] === '[[at(document.type, "product")]]' ||
-      queries[0].match(/[[at(document.id, ".*")]]/)
-    ) {
-      if (queries[1] && queries[1] === '[[at(document.tags, ["Featured"])]]') {
+
+    if (query === '[[at(document.type, "product")]]') {
+      if (options === '[[at(document.tags, ["Featured"])]]') {
         return res(ctx.json(featuredProducts));
       }
+
+      if (/\[\[fulltext\(document, ".*"\)\]\]/.test(options)) {
+        const searchString = options.substring(
+          options.indexOf('"') + 1,
+          options.lastIndexOf('"')
+        );
+        const filteredProducts = JSON.parse(JSON.stringify(products));
+        filteredProducts.results = products.results.filter((product) =>
+          new RegExp(searchString, "i").test(JSON.stringify(product.data))
+        );
+        return res(ctx.json(filteredProducts));
+      }
+
       return res(ctx.json(products));
     }
+
+    if (/\[\[any\(my\.product\.category, \[.*\]\)\]\]/.test(query)) {
+      const filterArray = query
+        .substring(query.indexOf('"'), query.lastIndexOf('"'))
+        .replaceAll('"', "")
+        .split(",");
+      const filteredProducts = JSON.parse(JSON.stringify(products));
+      filteredProducts.results = products.results.filter((product) =>
+        filterArray.includes(product.data.category.id)
+      );
+      return res(ctx.json(filteredProducts));
+    }
+
+    if (/\[\[at\(document\.id, ".*"\)\]\]/.test(query)) {
+      const productID = query.substring(
+        query.indexOf('"') + 1,
+        query.lastIndexOf('"')
+      );
+      const foundProduct = JSON.parse(JSON.stringify(products));
+      foundProduct.results = products.results.filter(
+        (product) => product.id === productID
+      );
+      return res(ctx.json(foundProduct));
+    }
+
     return res(ctx.status(404));
   }),
 ];
