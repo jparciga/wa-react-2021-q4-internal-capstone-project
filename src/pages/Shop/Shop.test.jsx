564e5b7cd92ed40148f080e79984d90d935c1174
Francisco Aceves <francisco.aceves@wizeline.com>
2021-12-17 10:12:13 -0600

    Added tests for the shop page

---
+++ src/pages/Shop/Shop.test.jsx
@@ -0,0 +1,44 @@
+import userEvent from "@testing-library/user-event";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "../../test/test-utils";
+import Shop from "./Shop";
+
+async function renderShop() {
+  render(<Shop />);
+  await waitForElementToBeRemoved(() => screen.queryAllByText(/loading/i));
+}
+
+const categories = require("../../mocks/en-us/product-categories.json");
+const products = require("../../mocks/en-us/products.json");
+
+test("Product Category Sidebar is fetching and rendering data from the API", async () => {
+  await renderShop();
+  expect(
+    screen.getByRole("link", { name: categories.results[0].data.name })
+  ).toBeInTheDocument();
+});
+
+test("Category links on Product Category Sidebar are filtering Products Grid correctly interacting with the API", async () => {
+  await renderShop();
+  const categoryName1 = categories.results[0].data.name;
+  const categoryName2 = categories.results[1].data.name;
+  expect(screen.getAllByText(categoryName1).length).toBeGreaterThan(1);
+  expect(screen.getAllByText(categoryName2).length).toBeGreaterThan(1);
+  userEvent.click(screen.getByRole("link", { name: categoryName1 }));
+  await waitForElementToBeRemoved(() => screen.queryAllByText(/loading/i));
+  expect(screen.getAllByText(categoryName1).length).toBeGreaterThan(1);
+  expect(screen.getAllByText(categoryName2)).toHaveLength(1);
+});
+
+test("Pagination Controls are generated correctly based on the number of results fetched from the API and the maximum number of products per page", async () => {
+  await renderShop();
+  for (let page = 1; page <= products.total_pages; page += 1) {
+    expect(screen.getByRole("link", { name: page })).toBeInTheDocument();
+  }
+  expect(
+    screen.queryByRole("link", { name: products.total_pages + 1 })
+  ).not.toBeInTheDocument();
+});
