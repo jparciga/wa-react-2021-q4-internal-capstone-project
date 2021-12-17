3ed6912a388d4597e38d0c630b92ca373322047c
Francisco Aceves <francisco.aceves@wizeline.com>
2021-12-17 08:18:46 -0600

    Added tests for the home page

---
+++ src/pages/Home/Home.test.jsx
@@ -0,0 +1,38 @@
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "../../test/test-utils";
+import Home from "./Home";
+
+async function renderHome() {
+  render(<Home />);
+  await waitForElementToBeRemoved(() => screen.queryAllByText(/loading/i));
+}
+
+const featuredBanners = require("../../mocks/en-us/featured-banners.json");
+
+test("Featured Banners Slider is fetching and rendering data from the API", async () => {
+  await renderHome();
+  expect(
+    screen.getByText(featuredBanners.results[0].data.title)
+  ).toBeInTheDocument();
+});
+
+const categories = require("../../mocks/en-us/product-categories.json");
+
+test("Categories Carousel/Grid is fetching and rendering data from the API", async () => {
+  await renderHome();
+  expect(
+    screen.getAllByText(categories.results[0].data.name)[0]
+  ).toBeInTheDocument();
+});
+
+const featuredProducts = require("../../mocks/en-us/featured-products.json");
+
+test("Featured Products Grid is fetching and rendering data from the API", async () => {
+  await renderHome();
+  expect(
+    screen.getByText(featuredProducts.results[0].data.name)
+  ).toBeInTheDocument();
+});
