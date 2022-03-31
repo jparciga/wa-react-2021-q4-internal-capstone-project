d808b7deaae1680597c6ea26c1c278d40154055a
AnaGuerreroH <ana.guerrero@wizeline.com>
2022-03-30 23:38:41 -0600

    Home

---
+++ src/home/carousel/carousel.js
@@ -0,0 +1,116 @@
+import React, { useEffect, useState } from "react";
+import './../../css/carousel.css'
+import { Image, Text, StyleSheet } from 'react-native';
+import myJson from './../../mocks/en-us/featured-banners.json'
+
+const styles = StyleSheet.create({
+    title:{
+        display:'inline-block',
+        alignItems:'center',
+        textAlign:'center',
+        fontFamily:"Roboto",
+        textTransform:'uppercase',
+        fontSize:'40px',
+        fontWeight:'bold',
+        color: '#fff'
+    },
+    description:{
+        display:'inline-block',
+        alignItems:'center',
+        textAlign:'center',
+        fontFamily:"Roboto",
+        fontSize:'18px',
+        color: '#fff'
+    }
+})
+
+export const CarouselItem = (props) => {
+    const {title, description, url, itemIndex, activeItem} = props
+    
+    var measure = 0
+    
+    if(itemIndex === activeItem){
+        measure = 0
+    } else if(activeItem===0 && itemIndex===myJson.results.length-1){
+        measure = -100
+    } else if(itemIndex===0 && activeItem===myJson.results.length-1){
+        measure = 100
+    } else {
+        const diff = itemIndex - activeItem
+        measure = diff * 100
+    }
+
+    return (
+        <div className="carousel-item" style={{
+            justifyContent: "center",
+            alignItems: "center",
+            width:'100%',
+            transform : `translateX(${measure}%)`,
+            backgroundColor: '#8019c8',
+            paddingTop: '10px',
+            paddingBottom: '10px'
+          }}>
+            <div className="titleContainer">
+                <Text style={styles.title}>{title}</Text>
+                <Text style={styles.description}>{description}</Text>
+            </div>
+            <Image style={{width: '100%', height: undefined, aspectRatio: 10/3}} source={url}/>
+        </div>
+    );
+};
+
+function Carousel() {
+    const [activeIndex, setActiveIndex] = useState(0)
+    const [paused, setPause] = useState(false)
+
+    const updateIndex = (newIndex) => {
+        if(newIndex<0){
+            newIndex = myJson.results.length-1
+        } else if(newIndex>= myJson.results.length){
+            newIndex = 0
+        }
+        setActiveIndex(newIndex)
+    }
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            if(!paused){
+                updateIndex(activeIndex + 1)
+            }
+        }, 3000);
+
+        return() => {
+            if(interval){
+                clearInterval(interval)
+            }
+        };
+    })
+
+    return (
+        <div className="carousel"
+            onMouseEnter={()=>setPause(true)}
+            onMouseLeave={()=>setPause(false)}
+        >
+            <div className="inner" style={{width: '100%'}}>
+                {myJson.results.map((item, index)=>{
+                    return(
+                        <CarouselItem title={item.data.title} description={item.data.description[0].text} url={item.data.main_image.url} itemIndex={index} activeItem={activeIndex}/>
+                    )
+                })}
+            </div>
+            <div className="indicators">
+                {
+                    myJson.results.map((item, index)=>{
+                        return(
+                            <span className={`${index === activeIndex? "active": ""}`} onClick={()=>{updateIndex(index)}} ></span>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
+};
+
+
+
+export default Carousel;
---
+++ src/home/categories/categories.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { FlatList, Text, StyleSheet, SafeAreaView, Image, ScrollView } from "react-native-web";
+import categories from './../../mocks/en-us/product-categories.json';
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        width: '100%'
+    },
+    title: {
+        fontSize: 32,
+    },
+    itemImage:{
+        width:'100%',
+        height:'undefined',
+        aspectRatio: 1/1
+    },
+    categoriesTitle:{
+        fontSize: '2rem',
+        fontWeight:'bold',
+        fontFamily:"Roboto"
+    },
+    itemContainer:{
+        width: '100%'
+    }
+})
+
+function ListCategories(){
+    const renderItem = ({item})=>(
+        <div style={{width: '100%', margin:'5px', backgroundColor: '#f5f5f5'}} title={item.data.name} url={item.data.main_image.url}>
+            <div>
+                <Image style={styles.itemImage} source={item.data.main_image.url}></Image>
+                <Text style={{color: '#8019c8', padding: '10px', fontSize:'1rem', fontWeight:'bold'}}>{item.data.name}</Text>
+            </div>
+        </div>
+    );
+
+    return(
+        <div className="categories" style={{paddingBottom: '45px'}}>
+            <div className='sectionTitle' style={{fontSize:'2rem', fontFamily:'Roboto', fontWeight: 'bold'}}>Categories</div>
+            <SafeAreaView styles={styles.container}>
+                <ScrollView horizontal contentContainerStyle={{flexGrow: 1, justifyContent: 'space-between'}}>
+                    <FlatList
+                        style={{width:'100%'}}
+                        contentContainerStyle={{margin:4}}
+                        horizontal={false}
+                        numColumns={5}
+                        data={categories.results}
+                        renderItem={renderItem}
+                        keyExtractor={(item)=>item.id}
+                        columnWrapperStyle={{flex: 1, justifyContent: 'space-evenly', width:'100%'}}
+                    />
+                </ScrollView>
+            </SafeAreaView>
+        </div>
+    );
+};
+
+export default ListCategories;
